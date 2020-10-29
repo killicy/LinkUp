db50ea9c96959140b3f326b970ca47c3bff220b2
Ubuntu <ubuntu@ip-172-26-6-90.ec2.internal>
2020-10-29 01:57:43 +0000

    Intial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <h1>This</h1>
+    );
+  }
+}
+
+export default App;
