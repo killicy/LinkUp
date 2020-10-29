112d9c741908c7a2d78a70365ac0413d517fcead
Jennifer Brown <killicy@gmail.com>
2020-10-28 22:46:20 -0400

    Update api.js with example

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ router.get('/message', (req, res) => {
 	res.send({message: "Hello from Express Mongo backend."});
 });
 
-// Examples
+// Example
 // router.get('/todos', function(req, res) {
 //     Login.find(function(err, todos) {
 //         if (err) {
@@ -15,50 +15,6 @@ router.get('/message', (req, res) => {
 //         } else {
 //             res.json(todos);
 //         }
-//     });
-// });
-
-// router.get('/todos/:id', function(req, res) {
-//     const id = req.params.id;
-//     Login.findById(id, function(err, todo) {
-//         res.json(todo);
-//     });
-// });
-
-// router.post('/todos/add', function(req, res) {
-//     const todo = new Todo(req.body);
-//     todo.save()
-//         .then(todo => {
-//             res.status(200).json({'todo': 'todo added successfully'});
-//         })
-//         .catch(err => {
-//             res.status(400).send('adding new todo failed');
-//         });
-// });
-
-// router.delete('/todos/delete/:id', function (req, res) {
-//     Todo.findByIdAndRemove(req.params.id, function (err) {
-//         if (err) return next(err);
-//         res.send('deleted successfully!');
-//     })
-// });
-
-// router.post('/todos/update/:id', function(req, res) {
-//     Todo.findById(req.params.id, function(err, todo) {
-//         if (!todo)
-//             res.status(404).send("data is not found");
-//         else
-//             todo.todoDescription = req.body.todoDescription;
-//             todo.todoResponsible = req.body.todoResponsible;
-//             todo.todoPriority = req.body.todoPriority;
-//             todo.todoCompleted = req.body.todoCompleted;
-
-//             todo.save().then(todo => {
-//                 res.json('Todo updated!');
-//             })
-//             .catch(err => {
-//                 res.status(400).send("Update not possible");
-//             });
 //     });
 // });
 
