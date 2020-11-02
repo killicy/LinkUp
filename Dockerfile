b9ee1a22608ca4d1305a32ff90778bc544daf4d3
Fauxfox01 <tumblingpebble@gmail.com>
2020-11-02 16:33:54 -0500

    ClientChanges

--- Dockerfile
+++ Dockerfile
@@ -17,3 +17,23 @@ COPY . /server/
 
 EXPOSE 5000
 CMD [ "npm", "run", "server" ]
+
+FROM mhart/alpine-node:10 as node
+
+# Create app directory
+WORKDIR /client
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json /client/
+
+RUN yarn install
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . /client/
+
+EXPOSE 3000
+CMD ["npm", "start"]
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from './logo.svg';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css';
 
-// import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Component {
-  render() {
-    return (
-      <h1>This</h1>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
