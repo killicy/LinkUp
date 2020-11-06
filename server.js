17b6139d6ada2d24af1c56813c4d07a9e3c116d1
AndrewUCF <70362777+AndrewUCF@users.noreply.github.com>
2020-11-05 20:03:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.use(express.json());
 // Cross Origin Requests
 app.use(cors());
 
+// headers
+app.use((req, res, next) => 
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
 // DB Config
 const dbConnection = keys.mongoURI;
 
